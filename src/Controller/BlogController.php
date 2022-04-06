777ed1916deba1a6233243e90b86114016265590
Wilmer Fabian Bermudez Martin <ingenierodesarrollo6@comersantander.local>
2022-04-06 12:48:11 -0500

    Guardado de mensaje de contacto

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +15,22 @@ class BlogController extends AbstractController
     public function index(): Response
     {
         return $this->render ('blog/index.html.twig');
+    }
+
+    /**
+     * @Route("/blog/crear", name="crear_entrada")
+     */
+    public function crearNoticia(): Response
+    {
+        return $this->render ('blog/crearNoti.html.twig');
+    }
+
+    /**
+     * @Route("/blog/guardar", name="guardar_noticia")
+     */
+    public function guardarNoticia(Request $request): Response
+    {
+        //dd($request);
+        return $this->render ('blog/crearNoti.html.twig');
     }
 }
--- src/Controller/ContactoController.php
+++ src/Controller/ContactoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Contacto;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +14,30 @@ class ContactoController extends AbstractController
      * @Route("/contacto", name="app_contacto")
      */
     public function index(): Response
-    {
+    {   
+        return $this->render('contacto/index.html.twig', [
+            'controller_name' => 'ContactoController',
+        ]);
+    }
+
+    /**
+     * @Route("/contacto/crear", name="mensaje_contacto")
+     */
+    public function mensajeContacto(Request $request): Response
+    {   
+        
+        $em = $this->getDoctrine()->getManager();
+        $conexion = $em->getConnection();
+        $formato =$request->request->all();
+        //dd($formato);
+        $mensaje = new Contacto;
+        $mensaje->setNombre($formato['nombre']);
+        $mensaje->setCorreo($formato['correo']);
+        $mensaje->setMensaje($formato['mensaje']);
+        $em->persist($mensaje);
+        //dd($mensaje);
+        $flush = $em->flush();
+
         return $this->render('contacto/index.html.twig', [
             'controller_name' => 'ContactoController',
         ]);
