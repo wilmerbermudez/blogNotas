5ff19a88b10e82048c5c161aa1dddb3a016201e4
Wilmer Fabian Bermudez Martin <ingenierodesarrollo6@comersantander.local>
2022-04-07 19:21:41 -0500

    Edicionn y ver noticia completas y organizar visualmente

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -59,10 +59,51 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/update/{id}", name="update_noticia")
      */
-    public function updateNoticia(Request $request): Response
-    {
-        
+    public function updateNoticia($id): Response
+    {   
+        $em = $this->getDoctrine()->getManager();
+        $conexion = $em->getConnection();
+        $noticias = $em->getRepository(Entrada::class)->find($id);
 
-        return $this->render('blog/index.html.twig');
+        return $this->render ('blog/updateNoti.html.twig', [
+            'noticia' => $noticias
+        ]);
+    }
+
+    /**
+     * @Route("/blog/actualizar/{id}", name="actualizar_noticia")
+     */
+    public function actualizarNoticia(Request $request, $id): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $conexion = $em->getConnection();
+        $formato =$request->request->all();
+        $noticias = $em->getRepository(Entrada::class)->find($id);
+        /* $archivo = $_FILES['imagen']['name'];
+        $temp = $_FILES['imagen']['tmp_name'];
+        move_uploaded_file($temp, 'images/'.$archivo); */
+        $noticias->setTitulo($formato['titulo']);
+        $noticias->setAutor('WILMER');
+        //$noticias->setImagen($archivo);
+        $noticias->setDescripcion($formato['noticia']);
+        $noticias->setFechaCreacion(new \DateTime());
+        $em->persist($noticias);
+        $flush = $em->flush();
+
+        return $this->redirectToRoute('app_blog');
+    }
+
+    /**
+     * @Route("/blog/ver/{id}", name="ver_noticia")
+     */
+    public function verNoticia($id): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $conexion = $em->getConnection();
+        $noticias = $em->getRepository(Entrada::class)->find($id);
+
+        return $this->render ('blog/noticia.html.twig', [
+            'noticia' => $noticias
+        ]);
     }
 }
